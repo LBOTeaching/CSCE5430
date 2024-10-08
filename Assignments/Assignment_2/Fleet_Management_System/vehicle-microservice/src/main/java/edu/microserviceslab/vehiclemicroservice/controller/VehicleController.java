4f95b143b044b6597b19ce4196e84be3f108df68
PraveenKumarSuggula <praveenkumar.suggula0@gmail.com>
2024-10-08 12:34:22 -0500

    Integrated Assignment - Microservices

---
+++ Assignments/Assignment_2/Fleet_Management_System/vehicle-microservice/src/main/java/edu/microserviceslab/vehiclemicroservice/controller/VehicleController.java
@@ -0,0 +1,30 @@
+package edu.microserviceslab.vehiclemicroservice.controller;
+
+import edu.microserviceslab.vehiclemicroservice.entity.Vehicle;
+import edu.microserviceslab.vehiclemicroservice.service.interfaces.VehicleService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicle")
+public class VehicleController {
+
+    private VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @ResponseBody
+    @RequestMapping("/list")
+    public List<Vehicle> listAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+
+    @ResponseBody
+    @RequestMapping("/licensePlate/{vehicleId}")
+    public String getVehicleLicensePlate(@PathVariable("vehicleId") Long vehicleId) {
+        return vehicleService.getVehicleLicensePlate(vehicleId);
+    }
+}
